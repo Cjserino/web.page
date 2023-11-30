338046a1476c7d15f6eae31d47ca762b9d92901e
Cjserino <152393833+Cjserino@users.noreply.github.com>
2023-11-29 15:26:41 -1200

    Update web.page.py

--- web.page.py
+++ web.page.py
@@ -5,3 +5,12 @@ st.set_page_config(page_title="My Webpage",page_icon=",tada:",layout="wide")
 st.subheader("Hi Im a Robot")
 st.title("Webpage")
 st.write("Welcome to my webpage")
+st.write("[Learn MOre >](https://www.youtube.com/)")
+
+with st.container():
+    st.write("---")
+    left_column, right_column=st.columns(2)
+    with left_column:
+        st.write("What I Do")
+        st.write("##")
+    
