cc6b4071f97c3a822f963f0fbad02398e6b27f57
Cjserino <152393833+Cjserino@users.noreply.github.com>
2023-11-29 15:09:14 -1200

    Create web.page.py

---
+++ web.page.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage",page_icon=",tada:",layout="wide")
+
+st.subheader("Hi Im a Robot")
+st.title("Webpage")
+st.write("Welcome to my webpage")
